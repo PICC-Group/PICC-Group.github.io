84285f747c56ecb24f8ae6ae3c774c9562066ab5
tbergkvist <bergkvist.teo@protonmail.com>
2024-02-10 13:34:20 +0100

    Updates.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     let blogPosts = [
-        { title: "9 Feb 2024", content: "We are open for sponsorships. PLease" },
+        { title: "9 Feb 2024", content: "We have started building the setup. We are also seeking sponsorships and grants to afford going to Florence and attend the conference." },
         // Add more blog posts here
     ];
 
