2a5a7b0be31d174cfedf6af48f620f9e588d00e1
tbergkvist <bergkvist.teo@protonmail.com>
2024-02-10 14:40:41 +0100

    Fix style.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     let blogPosts = [
-        { title: "9 Feb 2024", content: "We have started building the setup. We are also seeking sponsorships and grants to afford going to Florence and attend the conference." },
+        { title: "9 Feb 2024", content: "We have started building the setup. We are also looking for sponsorships and grants to afford going to Florence and attend the conference." },
+        { title: "8 Feb 2024", content: "We, The PICC Group from Lund University, are happy to announce that we have been selected as one of the 6 finalists in the IEEE AP-S Student design contest."},
         // Add more blog posts here
     ];
 
