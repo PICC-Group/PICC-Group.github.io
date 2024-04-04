129a3cf329a0d61b4e4aad767a661a8fc3543cd1
tbergkvist <bergkvist.teo@protonmail.com>
2024-04-04 16:34:47 +0200

    Update blog.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     let blogPosts = [
-        { title: "9 Feb 2024", content: "We have started building the setup. We are also looking for sponsorships and grants to afford going to Florence and attend the conference." },
+        { title: "4 April 2024", content: "The development of hardware and software is progressing. Long nights and huge amounts of RedBull = impressive results! Looking forward to revealing our project this summer! <img src='images/hax.jpg' class='blogpic' alt='nerds'>"},
+        { title: "9 Feb 2024", content: "We have started building the setup. We are also looking for sponsorships and grants to afford going to Florence and attend the conference. <img src='images/olhax.jpg' class='blogpic' alt='nerds'>" },
         { title: "8 Feb 2024", content: "We, The PICC Group from Lund University, are happy to announce that we have been selected as one of the 6 finalists in the IEEE AP-S Student design contest."},
         // Add more blog posts here
     ];
